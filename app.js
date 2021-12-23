21adc924dd5fb465f431e5c617df145c178f387d
vohoang2005 <vohoang2005@gmail.com>
2021-12-23 11:47:32 -0800

    updated app.js

--- app.js
+++ app.js
@@ -10,12 +10,47 @@ const inquirer = require('inquirer')
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // }); 
 
-inquirer
-  .prompt([
+const promptProject = () => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
     }
-  ])
-  .then(answers => console.log(answers));
+  ]);
+};
+
+promptProject().then(answers => console.log(answers));
